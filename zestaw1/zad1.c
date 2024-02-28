daa62aca7c37834b5f844d72002dbdc6ef04eaee
Szlonikk <jakubbednarz31@gmail.com>
2024-02-28 13:03:15 +0100

    zadanie 1 skonczone

--- zestaw1/zad1.c
+++ zestaw1/zad1.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+void drukuj(int *tab, int i){
+    int j=0;
+    for(;j<i;j++){
+        if(tab[j]>10 && tab[j]<100){
+            printf("%d, ",tab[j]);
+        }
+    }
+
+}
+
 int main(){
-
-
+    int tab[50];
+    int i=0;
+    int odczyt=-1;
+    for(;i<50 && odczyt!=0;i++){
+        scanf("%d",&odczyt);
+        tab[i]=odczyt;
+    }
+    
+    drukuj(tab,i);
     return 0;
 }
