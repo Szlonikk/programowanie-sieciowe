15190e7504b52060ed8779405f1569e49eadf288
Szlonikk <jakubbednarz31@gmail.com>
2024-03-01 22:03:37 +0100

    poprawienie testowania w zad3

--- zestaw1/zad3.c
+++ zestaw1/zad3.c
@@ -15,11 +15,17 @@ bool printable_buf(const void * buf, int len){
 int main(){
 
     const char buff[19]="testowanko numer 1\0";
+    const char buff2[] = "niedrukowalny\7 znak\0";
 
     if(printable_buf(buff,18)){
         printf("Zawiera same drukowalne znaki");
     }else{
         printf("Zawiera jakieś niedrukowalne znaki");
     }
-
+    printf("\n");
+    if(printable_buf(buff2,19)){
+        printf("Zawiera same drukowalne znaki");
+    }else{
+        printf("Zawiera jakies niedrukowalne znaki");
+    }
 }
