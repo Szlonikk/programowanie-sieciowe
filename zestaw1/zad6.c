43b00be2d5a60e4c678a358e9f395b84fa778564
Szlonikk <jakubbednarz31@gmail.com>
2024-03-04 17:52:01 +0100

    pliki tekstowe

--- zestaw1/zad6.c
+++ zestaw1/zad6.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
@@ -8,16 +7,16 @@ int main (int argc, char* argv[]) {
 	int odczyt;
 	
 	if ((odczyt = open (argv[1], O_RDONLY, 0644)) == -1) {
-		printf ("Błąd otwarcia pliku %s do odczytu", argv[2]);
-		perror ("Błąd pliku");
+		printf ("Blad otwarcia pliku %s do odczytu", argv[1]);
+		perror ("Blad pliku");
 		exit (EXIT_FAILURE);
 	}
 	
 	int zapis;
 	
 	if ((zapis = open (argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
-		printf ("Błąd otwarcia pliku %s do zapisu", argv[2]);
-		perror ("Błąd pliku");
+		printf ("Blad otwarcia pliku %s do zapisu", argv[2]);
+		perror ("Blad pliku");
 		exit (EXIT_FAILURE);
 	}
 	
@@ -29,14 +28,14 @@ int main (int argc, char* argv[]) {
 		
 		odczytano = read(odczyt, &bufor, sizeof(bufor));
 		if (odczytano == -1) {
-			perror("Błąd odczytu z pliku\n");
+			perror("Blad odczytu z pliku\n");
 			exit(EXIT_FAILURE);
 		} else if (odczytano == 0) {
 			break;
 		} else {
 			zapisano = write (zapis, &bufor, odczytano);
 			if (zapisano == -1) {
-				perror("Błąd zapisu do pliku\n");
+				perror("Blad zapisu do pliku\n");
 				exit(EXIT_FAILURE);
 			}
 		}
