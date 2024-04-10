41c01d3d4fe15605095d9e27c8918707b1a6c271
Szlonikk <jakubbednarz31@gmail.com>
2024-04-10 10:38:07 +0200

    uzywanie epoll

--- zestaw4/tcp_serwer.cpp
+++ zestaw4/tcp_serwer.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <thread>
 #include <vector>
 #include <algorithm>
 #include <cctype>
@@ -9,9 +8,11 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/epoll.h>
 
 #define PORT 2020
 #define BUFFER_SIZE 1026 // 1024 for data + 2 for "\r\n"
+#define MAX_EVENTS 10
 
 bool isPalindrome(const std::string& word) {
     int length = word.length();
@@ -35,36 +36,38 @@ bool isLineValid(const std::string& line) {
     });
 }
 
-void handleClient(int clientSocket) {
+void processClientData(int clientSocket) {
     char buffer[BUFFER_SIZE];
-    while (true) {
-        memset(buffer, 0, BUFFER_SIZE);
-        ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 2, 0);
+    memset(buffer, 0, BUFFER_SIZE);
+    ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 2, 0);
 
-        std::string data(buffer, bytesRead);
-        std::string response;
-
-        if (!isLineValid(data)) {
-            response = "ERROR\r\n";
-        } else {
-            std::istringstream iss(data);
-            std::string word;
-            int words = 0, palindromes = 0;
-            while (iss >> word) {
-                words++;
-                if (isPalindrome(word)) {
-                    palindromes++;
-                }
-            }
-            std::ostringstream oss;
-            oss << palindromes << "/" << words << "\r\n";
-            response = oss.str();
-        }
-
-        send(clientSocket, response.c_str(), response.length(), 0);
+    if (bytesRead <= 0) {
+        // Client closed connection or error occurred.
+        close(clientSocket);
+        return;
     }
 
-    close(clientSocket);
+    std::string data(buffer, bytesRead);
+    std::string response;
+
+    if (!isLineValid(data)) {
+        response = "ERROR\r\n";
+    } else {
+        std::istringstream iss(data);
+        std::string word;
+        int words = 0, palindromes = 0;
+        while (iss >> word) {
+            words++;
+            if (isPalindrome(word)) {
+                palindromes++;
+            }
+        }
+        std::ostringstream oss;
+        oss << palindromes << "/" << words << "\r\n";
+        response = oss.str();
+    }
+
+    send(clientSocket, response.c_str(), response.length(), 0);
 }
 
 int main() {
@@ -77,6 +80,9 @@ int main() {
         perror("ERROR socket()");
         exit(EXIT_FAILURE);
     }
+
+    int opt = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
@@ -93,14 +99,45 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    int epfd = epoll_create1(0);
+    if (epfd == -1) {
+        perror("ERROR epoll_create1()");
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event ev, events[MAX_EVENTS];
+    ev.events = EPOLLIN;
+    ev.data.fd = serverSocket;
+
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, serverSocket, &ev) == -1) {
+        perror("ERROR epoll_ctl: serverSocket");
+        exit(EXIT_FAILURE);
+    }
+
     while (true) {
-        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
-        if (clientSocket < 0) {
-            perror("ERROR accept()");
-            continue;
+        int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        if (nfds == -1) {
+            perror("ERROR epoll_wait");
+            exit(EXIT_FAILURE);
         }
 
-        std::thread(handleClient, clientSocket).detach();
+        for (int n = 0; n < nfds; ++n) {
+            if (events[n].data.fd == serverSocket) {
+                clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+                if (clientSocket < 0) {
+                    perror("ERROR accept()");
+                    continue;
+                }
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = clientSocket;
+                if (epoll_ctl(epfd, EPOLL_CTL_ADD, clientSocket, &ev) == -1) {
+                    perror("ERROR epoll_ctl: clientSocket");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                processClientData(events[n].data.fd);
+            }
+        }
     }
 
     close(serverSocket);
