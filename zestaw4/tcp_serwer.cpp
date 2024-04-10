0296c31d8f17b5c81a4531ab81e16c13efb9ecb0
Szlonikk <jakubbednarz31@gmail.com>
2024-04-10 10:52:48 +0200

    przerobienie lineValid

--- zestaw4/tcp_serwer.cpp
+++ zestaw4/tcp_serwer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <thread>
 #include <vector>
 #include <algorithm>
 #include <cctype>
@@ -8,11 +9,9 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/epoll.h>
 
 #define PORT 2020
 #define BUFFER_SIZE 1026 // 1024 for data + 2 for "\r\n"
-#define MAX_EVENTS 10
 
 bool isPalindrome(const std::string& word) {
     int length = word.length();
@@ -25,49 +24,59 @@ bool isPalindrome(const std::string& word) {
 }
 
 bool isLineValid(const std::string& line) {
-    if (line.front() == ' ' || line.back() == ' ') {
+    std::string trimmed = line;
+    trimmed.erase(std::remove(trimmed.begin(), trimmed.end(), '\r'), trimmed.end());
+    trimmed.erase(std::remove(trimmed.begin(), trimmed.end(), '\n'), trimmed.end());
+
+    if (trimmed.front() == ' ' || trimmed.back() == ' ') {
         return false;
     }
-    if (line.find("  ") != std::string::npos) {
+    if (trimmed.find("  ") != std::string::npos) {
         return false;
     }
-    return std::all_of(line.begin(), line.end(), [](char c) {
+    return std::all_of(trimmed.begin(), trimmed.end(), [](char c) {
         return isalpha(c) || c == ' ';
     });
 }
 
-void processClientData(int clientSocket) {
+void handleClient(int clientSocket) {
     char buffer[BUFFER_SIZE];
-    memset(buffer, 0, BUFFER_SIZE);
-    ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 2, 0);
+    while (true) {
+        memset(buffer, 0, BUFFER_SIZE);
+        ssize_t bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 2, 0);
 
-    if (bytesRead <= 0) {
-        // Client closed connection or error occurred.
-        close(clientSocket);
-        return;
-    }
-
-    std::string data(buffer, bytesRead);
-    std::string response;
-
-    if (!isLineValid(data)) {
-        response = "ERROR\r\n";
-    } else {
-        std::istringstream iss(data);
-        std::string word;
-        int words = 0, palindromes = 0;
-        while (iss >> word) {
-            words++;
-            if (isPalindrome(word)) {
-                palindromes++;
-            }
+        if (bytesRead <= 0) {
+            break;
         }
-        std::ostringstream oss;
-        oss << palindromes << "/" << words << "\r\n";
-        response = oss.str();
+
+        std::string data(buffer, bytesRead);
+        if (data.substr(data.length() - 2) != "\r\n") {
+            data += "\r\n";
+        }
+
+        std::string response;
+
+        if (!isLineValid(data)) {
+            response = "ERROR\r\n";
+        } else {
+            std::istringstream iss(data);
+            std::string word;
+            int words = 0, palindromes = 0;
+            while (iss >> word) {
+                words++;
+                if (isPalindrome(word)) {
+                    palindromes++;
+                }
+            }
+            std::ostringstream oss;
+            oss << palindromes << "/" << words << "\r\n";
+            response = oss.str();
+        }
+
+        send(clientSocket, response.c_str(), response.length(), 0);
     }
 
-    send(clientSocket, response.c_str(), response.length(), 0);
+    close(clientSocket);
 }
 
 int main() {
@@ -80,9 +89,6 @@ int main() {
         perror("ERROR socket()");
         exit(EXIT_FAILURE);
     }
-
-    int opt = 1;
-    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
@@ -99,45 +105,16 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    int epfd = epoll_create1(0);
-    if (epfd == -1) {
-        perror("ERROR epoll_create1()");
-        exit(EXIT_FAILURE);
-    }
-
-    struct epoll_event ev, events[MAX_EVENTS];
-    ev.events = EPOLLIN;
-    ev.data.fd = serverSocket;
-
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, serverSocket, &ev) == -1) {
-        perror("ERROR epoll_ctl: serverSocket");
-        exit(EXIT_FAILURE);
-    }
 
     while (true) {
-        int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
-        if (nfds == -1) {
-            perror("ERROR epoll_wait");
-            exit(EXIT_FAILURE);
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        if (clientSocket < 0) {
+            perror("ERROR accept()");
+            continue;
         }
 
-        for (int n = 0; n < nfds; ++n) {
-            if (events[n].data.fd == serverSocket) {
-                clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
-                if (clientSocket < 0) {
-                    perror("ERROR accept()");
-                    continue;
-                }
-                ev.events = EPOLLIN | EPOLLET;
-                ev.data.fd = clientSocket;
-                if (epoll_ctl(epfd, EPOLL_CTL_ADD, clientSocket, &ev) == -1) {
-                    perror("ERROR epoll_ctl: clientSocket");
-                    exit(EXIT_FAILURE);
-                }
-            } else {
-                processClientData(events[n].data.fd);
-            }
-        }
+        
+        std::thread(handleClient, clientSocket).detach();
     }
 
     close(serverSocket);
